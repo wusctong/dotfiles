e28491d39520b6360c52b1c5a7dcaf024f7f951a
wusctong <wutongtree0513@gmail.com>
2026-04-09 21:48:21 +0800

    Improve mac fish config

--- MACOS/.config/fish/config.fish
+++ MACOS/.config/fish/config.fish
@@ -1,22 +1,20 @@
+fish_add_path ~/.local/bin
+fish_add_path ~/.npm-global/bin
+fish_add_path ~/Developer/devPATH
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/Cellar/zigup/2025.05.24/bin
+
+set -x PKG_CONFIG_PATH \
+    (brew --prefix)/lib/pkgconfig \
+    (brew --prefix)/share/pkgconfig \
+    $PKG_CONFIG_PATH
+
+zoxide init fish | source
+starship init fish | source
+
 function fish_greeting
-    set -gx PATH $PATH ~/.local/bin
-    set -gx PATH $PATH ~/.npm-global/bin
-    set -gx PATH $PATH ~/Developer/devPATH
-    set -gx PATH $PATH /opt/homebrew/bin
-    set -gx PATH $PATH /opt/homebrew/sbin
-    set -gx PATH $PATH /opt/homebrew/Cellar/zigup/2025.05.24/bin
-
-    set -gx PKG_CONFIG_PATH $PKG_CONFIG_PATH (brew --prefix opencv)/lib/pkgconfig
-    set -gx PKG_CONFIG_PATH $PKG_CONFIG_PATH (brew --prefix raylib)/lib/pkgconfig
-    # set -gx CGO_CXXFLAGS "-DGOCV_NO_OPAQUE_HANDLES"
-    # set -gx CGO_LDFLAGS "-Wl,-allow_multiple_definition"
-
-    zoxide init fish | source
-    starship init fish | source
-    fish_config theme choose "Gruvbox Dark"
-
+    fish_config theme choose "Catppuccin Mocha" 2>/dev/null
     clear
     fastfetch
-    # cat ~/Developer/c++/lambdafetch/ascii.txt
-    # echo (set_color yellow)"Welcome to Fish "(set_color red --bold)"<"(set_color yellow)">"(set_color bryellow)"<"(set_color normal)"."
 end
