20a9b605f973c3194548abca52d67a75c9b6e0a0
wusctong <wutongtree0513@gmail.com>
2026-04-10 18:38:27 +0800

    Correct linux fish config

--- LINUX/.config/fish/config.fish
+++ LINUX/.config/fish/config.fish
@@ -1,5 +1,17 @@
+fish_add_path /opt/cuda/bin
+fish_add_path ~/.cargo/bin
+
+set -x LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
+set -x LIBGL_ALWAYS_INDIRECT 0
+set -x __GLX_VENDOR_LIBRARY_NAME nvidia
+set -x GALLIUM_DRIVER d3d12
+set -x LIBVA_DRIVER_NAME d3d12
+
+zoxide init fish | source
+starship init fish | source
+
 function fish_greeting
-    set -gx PATH $PATH /opt/nvim-linux-x86_64/bin
-    starship init fish | source
+    fish_config theme choose catppuccin-mocha --color-theme=dark
+    clear
     fastfetch
 end
