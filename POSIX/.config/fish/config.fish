fa86892156c393b7a0eacf04acddfee7e75e7894
wusctong <wutongtree0513@gmail.com>
2026-05-30 16:57:19 +0800

    Fix starship cursor

--- POSIX/.config/fish/config.fish
+++ POSIX/.config/fish/config.fish
@@ -3,7 +3,7 @@ fish_add_path ~/.npm-global/bin
 fish_add_path ~/Developer/devPATH
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
-fish_add_path /opt/homebrew/Cellar/zigup/2025.05.24/bin
+fish_add_path ~/.cargo/bin
 
 set -x PKG_CONFIG_PATH \
     (brew --prefix)/lib/pkgconfig \
@@ -12,6 +12,10 @@ set -x PKG_CONFIG_PATH \
 
 zoxide init fish | source
 starship init fish | source
+
+function starship_cursor_fix --on-event fish_prompt
+    echo -ne '\e[2 q'
+end
 
 function fish_greeting
     fish_config theme choose catppuccin-mocha
